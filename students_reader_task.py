691b748c31087f5d83f91d20b7dc9fc350b40b4c
l1can13 <a.a.soroka05@gmail.com>
2023-10-06 13:15:51 +0400

    json done

--- students_reader_task.py
+++ students_reader_task.py
@@ -1,48 +1,68 @@
+import datetime
 from typing import Union, List, Dict
 from collections import namedtuple
-from datetime import date
+from datetime import date, datetime
 import os.path
 import json
-from datetime import time
-
 
 LAB_WORK_SESSION_KEYS = ("presence", "lab_work_n", "lab_work_mark", "date")
 STUDENT_KEYS = ("unique_id", "name", "surname", "group", "subgroup", "lab_works_sessions")
 
 
 class LabWorkSession:
-    __slots__ = ('_presence', '_l_w_number', '_l_w_mark', '_date')
+    """
+    Информация о лабораторном занятии, которое могло или не могло быть посещено студентом
+    """
 
-    def __init__(self, p: bool, l_w_n: int, l_w_m: int, d: date):
-        self._presence: bool = False
-        self._l_w_number: int = -1
-        self._l_w_mark: int = -1
-        self._date: Union[None, date] = None
-        if not self._build_session(p, l_w_n, l_w_m, d):
-            raise ValueError(f"LabWorkSession::"
-                             f"incorrect args:\npresence {p},\nl_w_number {l_w_n},\nl_w_mark {l_w_m},\ndate {d}")
+    def __init__(self, presence: bool, lab_work_number: int, lab_work_mark: int, lab_work_date: date):
+        if not self._validate_args(presence, lab_work_number, lab_work_mark, lab_work_date):
+            raise ValueError(f"LabWorkSession ::"
+                             f"incorrect args :\n"
+                             f"presence       : {presence},\n"
+                             f"lab_work_number: {lab_work_number},\n"
+                             f"lab_work_mark  : {lab_work_mark},\n"
+                             f"lab_work_date  : {lab_work_date}")
 
-    def _build_session(self, p: bool, l_w_n: int, l_w_m: int, d: date) -> bool:
-        '''
-        :param p:
-        :param l_w_n:
-        :param l_w_m:
-        :param d:
-        :return:
-        '''
-        if not p and l_w_n != -1:
+        self.presence = presence
+        self.lab_work_number = lab_work_number
+        self.lab_work_mark = lab_work_mark
+        self.lab_work_date = lab_work_date
+
+    @staticmethod
+    def _validate_args(presence: bool, lab_work_number: int, lab_work_mark: int, lab_work_date: date) -> bool:
+        """
+            param: presence: присутствие студента на л.р.(bool)
+            param: lab_work_number: номер л.р.(int)
+            param: lab_work_mark: оценка за л.р.(int)
+            param: lab_work_date: дата л.р.(date)
+        """
+        if not presence and lab_work_number != -1:
             return False
-        if l_w_n == -1 and l_w_m != -1:
+        if lab_work_number == -1 and lab_work_mark != -1:
             return False
-        if d is None and l_w_n != -1:
+        if lab_work_date is None and lab_work_number != -1:
             return False
-        if d is None and l_w_m != -1:
+        if lab_work_date is None and lab_work_mark != -1:
             return False
-        self._presence = p
-        self._l_w_number = l_w_n
-        self._l_w_mark = l_w_m
-        self._date = d
         return True
+
+    def __str__(self) -> str:
+        """
+            Строковое представление LabWorkSession
+            Пример:
+            {
+                    "presence":      1,
+                    "lab_work_n":    4,
+                    "lab_work_mark": 3,
+                    "date":          "15:12:23"
+            }
+        """
+        return f"\t\t{{\n" \
+               f"\t\t\t\"presence\":      {int(self.presence)},\n" \
+               f"\t\t\t\"lab_work_n\":    {self.lab_work_number},\n" \
+               f"\t\t\t\"lab_work_mark\": {self.lab_work_mark},\n" \
+               f"\t\t\t\"date\":          \"{self.lab_work_date.strftime('%d:%m:%y')}\"\n" \
+               f"\t\t}}"
 
     @property
     def presence(self) -> bool:
@@ -50,88 +70,132 @@ class LabWorkSession:
 
     @property
     def lab_work_number(self) -> int:
-        return self._l_w_number
+        return self._lab_work_number
 
     @property
     def lab_work_mark(self) -> int:
-        return self._l_w_mark
+        return self._lab_work_mark
 
     @property
     def lab_work_date(self) -> date:
-        return self._date
+        return self._lab_work_date
 
     @lab_work_number.setter
     def lab_work_number(self, val: int) -> None:
         if not isinstance(val, int):
             raise ValueError(f"LabWorkSession::lab_work_number::setter arg val error... Type {type(val)} is incorrect")
-        if not self.presence:
+        if not self._presence:
             raise ValueError(f"LabWorkSession::lab_work_number::new state invalid... Presence is false")
-        self._l_w_number = max(1, val)
+        self._lab_work_number = max(1, val)
 
     @lab_work_mark.setter
     def lab_work_mark(self, val: int) -> None:
         if not isinstance(val, int):
             raise ValueError(f"LabWorkSession::lab_work_mark::setter arg val error... Type {type(val)} is incorrect")
-        if not self.presence:
+        if not self._presence:
             raise ValueError(f"LabWorkSession::lab_work_mark::new state invalid... Presence is false")
-        if self.lab_work_number == -1:
+        if self._lab_work_number == -1:
             raise ValueError(f"LabWorkSession::lab_work_mark::new state invalid... Lab work number is -1")
-        self._l_w_mark = max(0, val)
+        self._lab_work_mark = max(0, val)
 
-    def __str__(self) -> str:
-        return f"\t\t{{\n"\
-               f"\t\t\t\"presence\":      {'1' if self.presence else '0'},\n"\
-               f"\t\t\t\"lab_work_n\":    {self.lab_work_number},\n"\
-               f"\t\t\t\"lab_work_mark\": {self.lab_work_mark},\n"\
-               f"\t\t\t\"date\":          \"{str(self.lab_work_date)}\"\n"\
-               f"\t\t}}"
+    @presence.setter
+    def presence(self, value):
+        self._presence = value
+
+    @lab_work_date.setter
+    def lab_work_date(self, value):
+        self._lab_work_date = value
 
 
 class Student:
     __slots__ = ('_unique_id', '_name', '_surname', '_group', '_subgroup', '_lab_work_sessions')
 
     def __init__(self, unique_id: int, name: str, surname: str, group: int, subgroup: int):
-        self._unique_id: int = -1
-        self._name:      str = "no name"
-        self._surname:   str = "no surname"
-        self._group:     int = 1
-        self._subgroup:  int = 1
-        if not self._build_student(unique_id, name, surname, group, subgroup):
+        """
+            param: unique_id: уникальный идентификатор студента (int)
+            param: name: имя студента (str)
+            param: surname: фамилия студента (str)
+            param: group: номер группы в которой студент обучается (int)
+            param: subgroup: номер подгруппы (int)
+        """
+        if not self._validate_args(unique_id, name, surname, group, subgroup):
             raise ValueError(f"Student::"
-                             f"incorrect args:\nunique_id, \nname {name},\nsurname {surname},\ngroup {group},\nsubgroup {subgroup}")
-        self._lab_work_sessions: List[LabWorkSession] = []
-
-    def _build_student(self, unique_id: int, name: str, surname: str, group: int, subgroup: int) -> bool:
-        if not isinstance(unique_id,  int) or unique_id < 0:
-            return False
-        # name
-        if not isinstance(name,  str):
-            return False
-        if len(name) == 0:
-            return False
-        # surname
-        if not isinstance(surname,  str):
-            return False
-        if len(surname) == 0:
-            return False
-        # group
-        if not isinstance(group,  int):
-            return False
-        if group <= 0:
-            return False
-        # group
-        if not isinstance(subgroup,  int):
-            return False
-        if subgroup <= 0:
-            return False
+                             f"incorrect args:\nname {name},\nsurname {surname},\ngroup {group},\nsubgroup {subgroup}")
         self._unique_id = unique_id
         self._name = name
         self._surname = surname
         self._group = group
         self._subgroup = subgroup
+        self._lab_work_sessions: List[LabWorkSession] = []
+
+    @staticmethod
+    def _validate_args(unique_id: int, name: str, surname: str, group: int, subgroup: int) -> bool:
+        """
+            param: unique_id: уникальный идентификатор студента (int)
+            param: name: имя студента (str)
+            param: surname: фамилия студента (str)
+            param: group: номер группы в которой студент обучается (int)
+            param: subgroup: номер подгруппы (int)
+        """
+        # name
+        if not isinstance(name, str):
+            return False
+        if len(name) == 0:
+            return False
+        # surname
+        if not isinstance(surname, str):
+            return False
+        if len(surname) == 0:
+            return False
+        # group
+        if not isinstance(group, int):
+            return False
+        if group <= 0:
+            return False
+        # group
+        if not isinstance(subgroup, int):
+            return False
+        if subgroup <= 0:
+            return False
         return True
 
     def __str__(self) -> str:
+        """
+        Строковое представление Student
+        Пример:
+        {
+                "unique_id":          26,
+                "name":               "Щукарев",
+                "surname":            "Даниил",
+                "group":              6408,
+                "subgroup":           2,
+                "lab_works_sessions": [
+                    {
+                        "presence":      1,
+                        "lab_work_n":    1,
+                        "lab_work_mark": 4,
+                        "date":          "15:9:23"
+                    },
+                    {
+                        "presence":      1,
+                        "lab_work_n":    2,
+                        "lab_work_mark": 4,
+                        "date":          "15:10:23"
+                    },
+                    {
+                        "presence":      1,
+                        "lab_work_n":    3,
+                        "lab_work_mark": 4,
+                        "date":          "15:11:23"
+                    },
+                    {
+                        "presence":      1,
+                        "lab_work_n":    4,
+                        "lab_work_mark": 3,
+                        "date":          "15:12:23"
+                    }]
+        }
+        """
         sep = ',\n'
         return (f"{{\n"
                 f"\t\"unique_id\":          {self.unique_id},\n"
@@ -139,59 +203,75 @@ class Student:
                 f"\t\"surname\":            \"{self.surname}\",\n"
                 f"\t\"group\":              {self.group},\n"
                 f"\t\"subgroup\":           {self.subgroup},\n"
-                f"\t\"lab_works_sessions\": [\n {sep.join(str(v)for v in self.lab_work_sessions)}]\n"
+                f"\t\"lab_works_sessions\": [\n {sep.join(str(v) for v in self.lab_work_sessions)}]\n"
                 f"}}")
-    
+
     @property
     def unique_id(self) -> int:
+        """
+        Метод доступа для unique_id
+        """
         return self._unique_id
 
     @property
     def group(self) -> int:
+        """
+        Метод доступа для номера группы
+        """
         return self._group
 
     @property
     def subgroup(self) -> int:
+        """
+        Метод доступа для номера подгруппы
+        """
         return self._subgroup
 
     @property
     def name(self) -> str:
+        """
+        Метод доступа для имени студента
+        """
         return self._name
 
     @property
     def surname(self) -> str:
+        """
+        Метод доступа для фамилии студента
+        """
         return self._surname
-    
-    @unique_id.setter
-    def unique_id(self, val) -> None:
-        if not isinstance(val, int):
-            raise ValueError(f"Student::unique_id::setter arg val error... Type {type(val)} is incorrect")
-        if val < 0:
-            raise ValueError(f"Student::unique_id::new state invalid... Unique id less then zero")
-        self._unique_id = val
 
     @name.setter
     def name(self, val: str) -> None:
-        if not isinstance(val, str):
-            raise ValueError(f"Student::name::setter arg val error... Type {type(val)} is incorrect")
-        if len(val) == 0:
-            raise ValueError(f"Student::name::new state invalid... Name is empty")
+        """
+        Метод для изменения значения имени студента
+        """
+        assert isinstance(val, str)
+        assert len(val) != 0
         self._name = val
 
     @surname.setter
     def surname(self, val: str) -> None:
-        if not isinstance(val, str):
-            raise ValueError(f"Student::surname::setter arg val error... Type {type(val)} is incorrect")
-        if len(val) == 0:
-            raise ValueError(f"Student::surname::new state invalid... Name is empty")
+        """
+        Метод для изменения значения фамилии студента
+        """
+        assert isinstance(val, str)
+        assert len(val) != 0
         self._name = val
 
     @property
     def lab_work_sessions(self):
+        """
+        Метод доступа для списка лабораторных работ, которые студент посетил или не посетил.
+        Использовать yield.
+        """
         for s in self._lab_work_sessions:
             yield s
 
     def append_lab_work_session(self, session: LabWorkSession):
+        """
+        Метод для регистрации нового лабораторного занятия
+        """
         self._lab_work_sessions.append(session)
 
 
@@ -204,11 +284,15 @@ def _load_lab_work_session(json_node) -> LabWorkSession:
     for key in LAB_WORK_SESSION_KEYS:
         if key not in json_node:
             raise KeyError(f"load_lab_work_session:: key \"{key}\" not present in json_node")
-    return LabWorkSession(True if int(json_node['presence']) == 1 else False,
-                          int(json_node['lab_work_n']),
-                          int(json_node['lab_work_mark']),
-                          time.fromisoformat(json_node['date'])
+
+    result = LabWorkSession(
+        bool(json_node['presence']),
+        int(json_node['lab_work_n']),
+        int(json_node['lab_work_mark']),
+        datetime.strptime(json_node['date'], "%d:%m:%y").date()
     )
+
+    return result
 
 
 def _load_student(json_node) -> Student:
@@ -226,7 +310,7 @@ def _load_student(json_node) -> Student:
         json_node['surname'],
         int(json_node['group']),
         int(json_node['subgroup'])
-        )
+    )
     for session in json_node['lab_works_sessions']:
         try:
             student.append_lab_work_session(_load_lab_work_session(session))
@@ -263,22 +347,7 @@ def load_students_csv(file_path: str) -> Union[List[Student], None]:
     assert isinstance(file_path, str)
     if not os.path.exists(file_path):
         return None
-    
-    with open(file_path, 'rt', encoding='utf-8') as input_file:
-        lines = input_file.readlines()
-
-    headers = lines[0].strip().split(';')
-    fields = []
-    students = []
-    for line in lines:
-        try:
-            line_lst = line.strip().split(';')
-            first = dict(zip(headers[:5], line_lst[:5]))
-            first['lab_works_sessions'] = dict(zip(headers[5:], line_lst[5:]))
-        except Exception as ex:
-            print(ex)
-            continue
-
+    ...
 
 
 def load_students_json(file_path: str) -> Union[List[Student], None]:
@@ -289,34 +358,41 @@ def load_students_json(file_path: str) -> Union[List[Student], None]:
     assert isinstance(file_path, str)
     if not os.path.exists(file_path):
         return None
+
+    students_raw: Dict[int, Student] = {}
+
     with open(file_path, 'rt', encoding='utf-8') as input_file:
-        raw_json = json.load(input_file)
-        if 'students' not in raw_json:
-            return None
-        students = []
-        for node in raw_json['students']:
-            try:
-                students.append(_load_student(node))
-            except Exception as ex:
-                print(ex)
-                continue
-        return students
+        try:
+            json_data = json.load(input_file)
+            students_list = json_data.get("students", [])
+
+            for student_data in students_list:
+                unique_id = student_data.get("unique_id")
+                if unique_id is not None:
+                    students_raw[unique_id] = _load_student(student_data)
+        except json.JSONDecodeError as ex:
+            print(ex)
+
+    return list(students_raw.values())
+
 
 def get_variable_name(var, namespace=None):
     if namespace is None:
         namespace = globals()
-    
+
     for name, value in namespace.items():
         if value is var:
             return name
     return None
 
+
 def save_students_json(file_path: str, students: List[Student]):
     """
     Запись списка студентов в json файл
     """
+
     assert isinstance(file_path, str)
-    
+
     with open(file_path, 'w', encoding='utf-8') as output_file:
         init = f'{{\n\"{get_variable_name(students)}\":[\n'
         output_file.write(init)
@@ -328,11 +404,12 @@ def save_students_json(file_path: str, students: List[Student]):
 
         output_file.write(']}')
 
+
 def save_students_csv(file_path: str, students: List[Student]):
     """
     Запись списка студентов в csv файл
     """
-
+    ...
 
 
 if __name__ == '__main__':
@@ -343,13 +420,12 @@ if __name__ == '__main__':
     # Задание на проверку csv читалки:
     # 1.-3. аналогично
 
-    # students = load_students_json('students.json')
-    # save_students_json('students_saved.json', students)
-    # students = load_students_json('students_saved.json')
-    
-    students = load_students_csv('students.csv')
+    students = load_students_json('students.json')
+    save_students_json('students_saved.json', students)
+
+    # students = load_students_csv('students.csv')
     # students = save_students_csv('students_saved.csv')
     # load_students_csv('students_saved.csv', students)
-    
-    # for s in students:
-        # print(s)
+
+    for s in students:
+        print(s)
